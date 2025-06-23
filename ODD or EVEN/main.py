d5cd69d5ab84a605de1b73195ecd5bb81903b31e
Thimira Kodithuwakku <thimirakodithuwakku14@gmail.com>
2025-06-23 06:27:14 +0530

    Number checker

---
+++ ODD or EVEN/main.py
@@ -0,0 +1,7 @@
+print("Welcome to number checker!")
+number = int(input("Enter your number: "))
+
+if number % 2 == 0:
+    print("Your number is an Even number.")
+else:
+    print("Your number is an ODD number.")
